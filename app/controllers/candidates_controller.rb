74da2a217688aac43fe61d8f498bdb27a28d56d1
AhernShaia <gemgt4gtii@gmail.com>
2023-02-09 19:30:03 +0800

    Candidate: Create

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,6 +1,27 @@
 class CandidatesController < ApplicationController
-
   def index
-    
+
   end
+
+  def new
+    @candidate = Candidate.new
+  end
+
+  def create
+    @candidate = Candidate.new(clean_params)
+
+    if @candidate.save
+      flash[:notice] = "新增成功"
+      redirect_to '/candidates'
+    else
+      render :new
+    end
+  end
+
+  private
+  def clean_params
+    params.require(:candidate).permit(:name, :age, :party, :politics)
+  end
+
+
 end
